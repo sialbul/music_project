c51e31a1ef3f7e2e747a0b349f2ef3e93c279780
Sibel <sbaslamisli@yahoo.com>
2021-08-21 16:37:11 -0400

    displayed the results

--- music_project/src/components/SearchBar.js
+++ music_project/src/components/SearchBar.js
@@ -3,20 +3,40 @@ import "../style/style.css";
 import axios from "axios";
 
 export default function SearchBar() {
-    const [term, setTerm] = useState(" ");
+    const [term, setTerm] = useState("korn");
+    const [results, setResults] = useState([]);
 
     useEffect(() => {
         const searchAudio = async () => {
-            await axios.get("https://itunes.apple.com/search", {
-                params: {
-                    term: term,
-                    country: "US",
-                    types: "artists"
+            const { data } = await axios.get(
+                "https://itunes.apple.com/search",
+                {
+                    params: {
+                        term: term,
+                        country: "US",
+                        types: "artists",
+                        media: "music",
+                        limit: 5
+                    }
                 }
-            });
+            );
+
+            setResults(data.results);
         };
         searchAudio();
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div className="renderedDiv">
+                <a href={result.collectionViewUrl} target="_black">
+                    <div className="header">{result.trackName}</div>
+                    {result.releaseDate}
+                    <img src={result.artworkUrl100} />
+                </a>
+            </div>
+        );
+    });
 
     return (
         <div className="searchContainer">
@@ -28,6 +48,7 @@ export default function SearchBar() {
                     onChange={(e) => setTerm(e.target.value)}
                 />
             </div>
+            <div>{renderedResults}</div>
         </div>
     );
 }
