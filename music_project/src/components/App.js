d67f567471a71b87519e45830a608891a3d605df
Sibel <sbaslamisli@yahoo.com>
2021-08-22 12:33:01 -0400

    sorted the result by date is done

--- music_project/src/components/App.js
+++ music_project/src/components/App.js
@@ -9,6 +9,7 @@ export default class App extends Component {
     render() {
         return (
             <div className="container">
+                <h2>Album Search</h2>
                 <SearchBar onFormSubmit={this.onTermSubmit} />
             </div>
         );
--- music_project/src/components/SearchBar.js
+++ music_project/src/components/SearchBar.js
@@ -2,11 +2,21 @@ import React, { useState, useEffect } from "react";
 import "../style/style.css";
 import axios from "axios";
 import moment from "moment";
+import Logo from "../itunes.png";
 
 export default function SearchBar() {
     const [term, setTerm] = useState("korn");
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
 
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
     useEffect(() => {
         const searchAudio = async () => {
             const { data } = await axios.get(
@@ -17,42 +27,49 @@ export default function SearchBar() {
                         country: "US",
                         types: "artists",
                         media: "music",
-                        limit: 5
+                        limit: 10,
+                        entity: "album",
+                        explicit: "yes"
                     }
                 }
             );
-
             setResults(data.results);
         };
         searchAudio();
-    }, [term]);
+    }, [debouncedTerm]);
 
-    const renderedResults = results.map((result) => {
-        const date = moment(`${result.releaseDate}`).format("DD/MM/YYYY");
-        return (
-            <div className="renderedDiv">
-                <a
-                    className="renderResult"
-                    href={result.collectionViewUrl}
-                    target="_black">
-                    <div className="header">{result.trackName}</div>
+    const renderedResults = results
+        .sort((a, b) => new Date(a.releaseDate) - new Date(b.releaseDate))
+        .map((result) => {
+            const date = moment(`${result.releaseDate}`).format("DD/MM/YYYY");
+            return (
+                <div className="renderedDiv" key={result.collectionId}>
+                    <a
+                        className="renderResult"
+                        href={result.collectionViewUrl}
+                        target="_black">
+                        <img alt="album" src={result.artworkUrl100} />
+                        <div className="header">{result.collectionName}</div>
+                    </a>
                     <div>{date}</div>
-                    <img src={result.artworkUrl100} />
-                </a>
-            </div>
-        );
-    });
+
+                    <hr />
+                </div>
+            );
+        });
 
     return (
         <div className="searchContainer">
             <div className="field">
-                <label>Album Search</label>
+                <img className="navLogo" src={Logo} />
                 <input
                     type="text"
                     value={term}
                     onChange={(e) => setTerm(e.target.value)}
                 />
             </div>
+            <h2 className="header">{term}</h2>
+            <hr />
             <div>{renderedResults}</div>
         </div>
     );
